be130a5122afc12b014e391cd2ec16dc8200b162
Abhinov Gogoi <abhinov-gogoi@users.noreply.github.com>
2021-02-17 11:46:38 +0530

    nested test class messages

--- src/test/java/io/javabrains/NestedTestClassesTest.java
+++ src/test/java/io/javabrains/NestedTestClassesTest.java
@@ -14,43 +14,45 @@ class NestedTestClassesTest {
     // for testing different edge cases in divide, we write separate test cases, and put inside a nested class
     // However, in this case a better approach is the AssertAllExampleTest
     @Nested
+    @DisplayName("Divide method")
     class TestDivide {
+        // refer to the message convention inside nested class
         @Test
-        @DisplayName("Test Divide function1")
+        @DisplayName("when dividing positive number with a positive number")
         void divide1() {
             int expected = 5;
             int actual = nestedTestClasses.divide(20, 4);
-            assertEquals(expected, actual, "The add method should divide two numbers");
+            assertEquals(expected, actual, "should return a correct positive number");
         }
 
         @Test
-        @DisplayName("Test Divide function2")
+        @DisplayName("when dividing negative number with a negative number")
         void divide2() {
             int expected = 5;
             int actual = nestedTestClasses.divide(-20, -4);
-            assertEquals(expected, actual, "The add method should divide two numbers");
+            assertEquals(expected, actual, "should return a correct positive number");
         }
 
         @Test
-        @DisplayName("Test Divide function3")
+        @DisplayName("when dividing positive number with a negative number")
         void divide3() {
             int expected = -5;
             int actual = nestedTestClasses.divide(20, -4);
-            assertEquals(expected, actual, "The add method should divide two numbers");
+            assertEquals(expected, actual, "should return a correct negative number");
         }
 
         @Test
-        @DisplayName("Test Divide function4")
+        @DisplayName("when dividing negative number with a positive number")
         void divide4() {
             int expected = -5;
             int actual = nestedTestClasses.divide(-20, 4);
-            assertEquals(expected, actual, "The add method should divide two numbers");
+            assertEquals(expected, actual, "should return a correct negative number");
         }
 
         @Test
-        @DisplayName("Test Divide function5")
+        @DisplayName("when dividing a number with a zero")
         void divide5() {
-            assertThrows(ArithmeticException.class, () -> nestedTestClasses.divide(20, 0), "The add method should divide two numbers");
+            assertThrows(ArithmeticException.class, () -> nestedTestClasses.divide(20, 0), "should throw exception");
         }
     }
 
